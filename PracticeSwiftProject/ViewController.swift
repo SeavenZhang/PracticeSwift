84f41a906c2b93ba3fdf212972c413f30ceb6318
Sky Zhang <skyzhang@iOSdeiMac.local>
2017-05-31 10:11:18 +0800

    Initial Commit

---
+++ PracticeSwiftProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeSwiftProject
+//
+//  Created by Sky Zhang on 2017/5/31.
+//  Copyright © 2017年 Sky.Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
